4d6ea801deb82b9705ef7714a77b84bbe8052551
amacoerp <deekshitha.cse5235@gmail.com>
2022-01-18 10:16:26 +0530

    10.16

--- app/Http/Controllers/Api/QuotationController.php
+++ app/Http/Controllers/Api/QuotationController.php
@@ -305,7 +305,7 @@ class QuotationController extends Controller
                         'quotation_id' => $quotation_id,
                         'total_amount' => $quotation_detail['total_amount'],
                         'analyse_id' => null,
-                        'product_id' => $quotation_detail['productId']?$quotation_detail['productId']:$product->id,
+                        'product_id' => $quotation_detail['productId']?$quotation_detail['productId']:isset($product)?$product->id:' ',
                         'purchase_price' => $quotation_detail['purchase_price'],
                         'description' => $quotation_detail['product_name']?$quotation_detail['product_name']:$quotation_detail['product'],
                         'product_description' => $quotation_detail['description'],
