f387a73fc74c0597e58dcad76f378d9534f72dea
amacoerp <amacoerp@gmail.com>
2022-01-21 14:54:06 +0530

    2.54PM

--- app/Http/Controllers/Api/RFQController.php
+++ app/Http/Controllers/Api/RFQController.php
@@ -270,8 +270,8 @@ class RFQController extends Controller
         // return $request;
         // try {
             $rfq->update([
-                'requested_date' => $request->requested_date,
-                'require_date' => $request->require_date,
+                'requested_date' => $request['requested_date'],
+                'require_date' => $request['require_date'],
                 // 'contact_id' => $request->contact_id,
             ]);
 
